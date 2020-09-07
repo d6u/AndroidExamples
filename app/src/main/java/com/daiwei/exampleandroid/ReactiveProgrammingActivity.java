2b5fb4f16c830665ec28d0f504a19cd84b52b482
Daiwei Lu <daiwei@fb.com>
2020-09-07 15:51:19 -0700

    commit 2

--- app/src/main/java/com/daiwei/exampleandroid/ReactiveProgrammingActivity.java
+++ app/src/main/java/com/daiwei/exampleandroid/ReactiveProgrammingActivity.java
@@ -6,29 +6,25 @@ import android.os.Looper;
 import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.daiwei.exampleandroid.reactiveprogramming.ReactiveProgrammingExampleOne;
 import io.reactivex.Single;
-import io.reactivex.SingleObserver;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.subjects.SingleSubject;
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class ReactiveProgrammingActivity extends AppCompatActivity {
 
-  private static final String TAG = ReactiveProgrammingActivity.class.getSimpleName();
+  public static final String LOG_TAG = ReactiveProgrammingActivity.class.getSimpleName();
 
   private final CompositeDisposable mCompositeDisposable = new CompositeDisposable();
   private final Handler mHandler = new Handler(Looper.getMainLooper());
-  private @Nullable TestClass mTestClass;
+  private @Nullable ReactiveProgrammingExampleOne mTestClass;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_reactive_programming);
 
-    mTestClass = new TestClass();
+    mTestClass = new ReactiveProgrammingExampleOne();
   }
 
   @Override
@@ -40,22 +36,22 @@ public class ReactiveProgrammingActivity extends AppCompatActivity {
 
     mHandler.postDelayed(() -> {
       Disposable disposable = single.subscribe(
-          v -> Log.d(TAG, "subscriber 1 got result: " + v),
-          t -> Log.e(TAG, "subscriber 1 got error", t));
+          v -> Log.d(LOG_TAG, "subscriber 1 got result: " + v),
+          t -> Log.e(LOG_TAG, "subscriber 1 got error", t));
       mCompositeDisposable.add(disposable);
     }, 500);
 
     mHandler.postDelayed(() -> {
       Disposable disposable = single.subscribe(
-          v -> Log.d(TAG, "subscriber 2 got result: " + v),
-          t -> Log.e(TAG, "subscriber 2 got error", t));
+          v -> Log.d(LOG_TAG, "subscriber 2 got result: " + v),
+          t -> Log.e(LOG_TAG, "subscriber 2 got error", t));
       mCompositeDisposable.add(disposable);
     }, 500);
 
     mHandler.postDelayed(() -> {
       Disposable disposable = single.subscribe(
-          v -> Log.d(TAG, "subscriber 3 got result: " + v),
-          t -> Log.e(TAG, "subscriber 3 got error", t));
+          v -> Log.d(LOG_TAG, "subscriber 3 got result: " + v),
+          t -> Log.e(LOG_TAG, "subscriber 3 got error", t));
       mCompositeDisposable.add(disposable);
     }, 3000);
   }
@@ -72,55 +68,4 @@ public class ReactiveProgrammingActivity extends AppCompatActivity {
     super.onDestroy();
   }
 
-  private static final class TestClass {
-
-    private final CompositeDisposable mCompositeDisposable = new CompositeDisposable();
-    private final Single<Integer> mSingle;
-
-    TestClass() {
-      mSingle = Single.just(1).cache();
-    }
-
-    void destroy() {
-      mCompositeDisposable.clear();
-    }
-
-    Single<String> doSomethingOnce() {
-      SingleSubject<String> subject = SingleSubject.create();
-
-      AtomicReference<Disposable> reference = new AtomicReference<>(null);
-
-      mSingle.flatMap(ignored -> {
-        Log.d(TAG, "Run hello logic");
-        return Single.timer(2, TimeUnit.SECONDS).map(ignored2 -> "Hello");
-      })
-          .doOnEvent((String v, Throwable t) -> {
-            Log.d(TAG, "Hello single event");
-            mCompositeDisposable.delete(reference.get());
-          })
-          .doOnDispose(() -> {
-            Log.d(TAG, "Hello single disposing");
-            subject.onError(new NoSuchElementException());
-          })
-          .subscribe(new SingleObserver<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-              reference.set(d);
-              mCompositeDisposable.add(d);
-            }
-
-            @Override
-            public void onSuccess(String s) {
-              subject.onSuccess(s);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-              subject.onError(e);
-            }
-          });
-
-      return subject;
-    }
-  }
 }
---
+++ app/src/main/java/com/daiwei/exampleandroid/reactiveprogramming/ReactiveProgrammingExampleOne.java
@@ -0,0 +1,64 @@
+package com.daiwei.exampleandroid.reactiveprogramming;
+
+import android.util.Log;
+import com.daiwei.exampleandroid.ReactiveProgrammingActivity;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.SingleSubject;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+public final class ReactiveProgrammingExampleOne {
+
+  private final CompositeDisposable mCompositeDisposable = new CompositeDisposable();
+  private final Single<Integer> mSingle;
+
+  public ReactiveProgrammingExampleOne() {
+    mSingle = Single.just(1).cache();
+  }
+
+  public void destroy() {
+    mCompositeDisposable.clear();
+  }
+
+  public Single<String> doSomethingOnce() {
+    SingleSubject<String> subject = SingleSubject.create();
+
+    AtomicReference<Disposable> reference = new AtomicReference<>(null);
+
+    mSingle.flatMap(ignored -> {
+      Log.d(ReactiveProgrammingActivity.LOG_TAG, "Run hello logic");
+      return Single.timer(2, TimeUnit.SECONDS).map(ignored2 -> "Hello");
+    })
+        .doOnEvent((String v, Throwable t) -> {
+          Log.d(ReactiveProgrammingActivity.LOG_TAG, "Hello single event");
+          mCompositeDisposable.delete(reference.get());
+        })
+        .doOnDispose(() -> {
+          Log.d(ReactiveProgrammingActivity.LOG_TAG, "Hello single disposing");
+          subject.onError(new NoSuchElementException());
+        })
+        .subscribe(new SingleObserver<String>() {
+          @Override
+          public void onSubscribe(Disposable d) {
+            reference.set(d);
+            mCompositeDisposable.add(d);
+          }
+
+          @Override
+          public void onSuccess(String s) {
+            subject.onSuccess(s);
+          }
+
+          @Override
+          public void onError(Throwable e) {
+            subject.onError(e);
+          }
+        });
+
+    return subject;
+  }
+}
